be60006c8ea3e53cef0c91ac821aabcbf9dd43a4
seewei <yongseewei@gmail.com>
2021-05-23 21:08:12 +0800

    test

--- .circleci/ansible/roles/setup/files/index.js
+++ .circleci/ansible/roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world! 234");
+res.end("Hello world! 2345");
 });
 server.listen(3000);
